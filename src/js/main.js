68339e6aa4b82c0fc471ac43ac3f6e2064ac0543
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-02-21 19:00:24 +0100

    Uppdaterat logotyp, fixat navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 "use strict";
 
+/* Javascript för att toggla darkmode och navigationsmenyn */
 const body = document.querySelector("body");
 const sidebar = body.querySelector(".sidebar");
 const toggle = body.querySelector(".toggle");
 const modeSwitch = body.querySelector(".toggle-switch");
 const modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
