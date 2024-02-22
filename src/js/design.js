e63f39e428afdce2fcf422794365185d7e96a47f
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-02-22 14:44:18 +0100

    Fungerande localstorage med dark/lightmode

--- src/js/design.js
+++ src/js/design.js
@@ -7,16 +7,45 @@ const toggle = body.querySelector(".toggle");
 const modeSwitch = body.querySelector(".toggle-switch");
 const modeText = body.querySelector(".mode-text");
 
+window.onload = checkMode();
+
+/* Sätta på darkmode och spara i localhost */
+function enableDarkMode() {
+    body.classList.add("dark");
+    modeText.innerText = "Light Mode";
+    localStorage.setItem("darkMode", "true");
+}
+
+/* Stänga av darkMode och spara i localhost */
+function disableDarkMode() {
+    body.classList.remove("dark");
+    modeText.innerText = "Dark Mode";
+    localStorage.setItem("darkMode", "false");
+}
+
+/* Navigationsmeny toggle */
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
+/* Togglar dark/light mode om man klickar på knapp. */
 modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
     if(body.classList.contains("dark")) {
-        modeText.innerText = "Light Mode";
+        disableDarkMode();
     } else {
-        modeText.innerText = "Dark Mode";
+        enableDarkMode();
     }
 })
+
+
+/* Tittar ifall darkMode är sparat som true eller ej i localstorage */
+function checkMode() {
+
+
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    if (isDarkMode) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+};
